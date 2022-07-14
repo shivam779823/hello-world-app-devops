4a2310610566a741c4beea7d67c284ac72719ce1
shivam <shivam@gmail.com>
2022-07-14 07:03:07 -0400

    g

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 
-@app.route("/")
-def main():
-    model = {"title": "Hello GCP."}
-    return render_template('index.html', model=model)
 
-if __name__ == "__main__":
+
+
+
+@app.route("/")
+def sample():
+    return render_template('index.html')
+
+
+@app.route("/fun/")
+def fun():
+    model = {"title": "Hey here is your fun Fact "}
+    return render_template('fun.html', model=model)
+
+@app.route("/Data")
+def data():
+    return render_template('student.html')
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+   if request.method == 'POST':
+      result = request.form
+      return render_template("result.html",result = result)    
+
+
+
+if __name__ == "__main__":   
     app.run(host='0.0.0.0', port=8080, debug=True, threaded=True)
